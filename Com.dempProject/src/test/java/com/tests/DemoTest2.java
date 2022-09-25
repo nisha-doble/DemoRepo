e669c6fcd76575190f5b61f4ba3eaf382c21e9eb
Tushar Doble <Tushar Doble@LAPTOP-KIV1JG4F>
2022-09-25 18:15:47 +0530

    adding a new test class

---
+++ Com.dempProject/src/test/java/com/tests/DemoTest2.java
@@ -0,0 +1,17 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	
+	@Test
+	public void test01() {
+		System.out.println("Hello world");
+	}
+	
+	@Test
+	public void test02() {
+		System.out.println("Hello world");
+	}
+}
